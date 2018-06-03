f46a54731e054fecd8b812fc9204de5a3d256bd1
matmandra <matmandra@int.pl>
2018-06-03 19:12:32 +0200

    Teraz program zapbiega wystąpieniu kilku znanych mi błędów, jak brak pliku txt.

--- ytdownload.py
+++ ytdownload.py
@@ -1,47 +1,67 @@
 ﻿from pytube import YouTube
+import os
 
-fo = open("location.txt", "r+")
-destination = fo.read();
-print ("Lokalizacja zapisu : ", destination)
-# Close opend file
-fo.close()
+def cls():
+	os.system("cls")
+##----------------------------------------------------------------------------------------------------------------------------#
 
-
+cls();
+try :
+	fo = open("location.txt", "r+")
+	path = fo.read();
+	fo.close()
+	print(" Lokalizacja zapisu - ", path)
+except (OSError, IOError) :
+	print(" \n Brak lokalizacji zapisu plików. \n Ustal Lokalizację")
+##----------------------------------------------------------------------------------------------------------------------------#
 def download_video() :
-	yt = YouTube(str(input("Podaj link video: ")))
-	videos = yt.streams.filter(progressive=True , file_extension='mp4').all()
+	cls();
+	try :
+		fo = open("location.txt", "r+")
+		destination = fo.read();
+		fo.close()
+	except (OSError, IOError) :	
+		print(" \n Brak lokalizacji zapisu plików. \n Ustal Lokalizację")
+		destination = None
 
-	s = 1
-	for v in videos:
-		print(str(s)+". "+str(v))
-		s += 1
+	if destination  is None :
+		print ("\n***************\n NO SAVE PATH\n***************")
+	else :
+		yt = YouTube(str(input(" Podaj link video: ")))
+		videos = yt.streams.filter(progressive=True , file_extension='mp4').all()
 
-	n = int(input("Wybierz numer porządanego video: "))
-	vid = videos[n-1]
+		s = 1
+		for v in videos:
+			print(str(s)+". "+str(v))
+			s += 1
 
-	vid.download(destination)
-	print(yt.title +"\nFile Has been successfully downloaded")
-	
+		n = int(input(" Wybierz numer porządanego video: "))
+		vid = videos[n-1]
+
+		vid.download(destination)
+		print(yt.title +"\nFile Has been successfully downloaded")
+##----------------------------------------------------------------------------------------------------------------------------#	
 def change_destination() :
+	cls();
 	fo = open("location.txt", "w")
 	destination2 = str(input("Wprowadz ścieżkę: ")) 
 	fo.write(destination2);
 	fo.close()
-	fo = open("location.txt", "r+")
-	destination = fo.read();
-	print ("Lokalizacja zapisu : ", destination)
-	fo.close()
-
+	
+##----------------------------------------------------------------------------------------------------------------------------#
 def main():
-	while True : 
-		print(" Menu \n 1 - pobieranie video \n 2 - zmiana lokalizacji zapisu \n 0 - EXIT ")
+	while True :
+		
+		print("\n MENU \n 1 - pobieranie video \n 2 - zmiana lokalizacji zapisu \n 0 - EXIT ")
 		wybierz = int(input("Opcja : "))
 		if wybierz == 1:
 			download_video();
 		if wybierz == 2:
 			change_destination();
 		if wybierz == 0:
+			cls();
 			break
-			
+##----------------------------------------------------------------------------------------------------------------------------#			
 if __name__ == '__main__':
         main()
+##----------------------------------------------------------------------------------------------------------------------------#
